e94dadeb9ac8f6b6cfbd68df6ea6dab5c9bc9786
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-19 14:10:25 +0200

    Add files via upload

---
+++ BK.py
@@ -0,0 +1,43 @@
+# testowanie na użytek własny
+from __future__ import unicode_literals
+import youtube_dl
+
+# podejście nr 1
+
+# ydl_opts = {
+#     "format": "bestaudio/best",
+#     "postprocessors": [
+#         {
+#             "key": "FFmpegExtractAudio",
+#             "preferredcodec": "mp3",
+#             "preferredquality": "192",
+#         }
+#     ],
+# }
+# with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#     ydl.download(["http://www.youtube.com/watch?v=BaW_jenozKc"])
+
+# podejście nr 2 (yt-dlp)
+
+import yt_dlp
+
+URLS = [
+    "https://www.youtube.com/watch?v=BaW_jenozKc",
+    "https://www.youtube.com/watch?v=xpYw22NaMUc&ab_channel=PrzemekGórczykPodcast",
+]
+
+try:
+    ydl_opts = {
+        "format": "m4a/bestaudio/best",
+        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        "postprocessors": [
+            {  # Extract audio using ffmpeg
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+            }
+        ],
+    }
+finally:
+    for i in URLS:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            error_code = ydl.download(i)
