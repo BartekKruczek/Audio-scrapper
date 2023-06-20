23bce7fdc32ad316913fddc5192f9c6280d6c9ec
JacobeCode <kuba.g.code.4@gmail.com>
2023-06-20 11:22:32 +0200

    First raw script for gpodcast working on main page

--- podcast_bsoup_dlp.py
+++ podcast_bsoup_dlp.py
@@ -22,32 +22,48 @@ dlp_scraper = yt_dlp.YoutubeDL(options)
 link_list_all = []
 title_list_all = []
 time_list_all = []
+url_main_list = []
+channels_title_list = []
 
 # Here is the input for creator URL profile
-URL_setup = "https://podcasts.google.com/feed/aHR0cHM6Ly93d3cuc3ByZWFrZXIuY29tL3Nob3cvMjg5NjMyMS9lcGlzb2Rlcy9mZWVk?sa=X&ved=0CHsQjs4CKARqFwoTCMiFuMyg0f8CFQAAAAAdAAAAABAB&hl=pl"
+URL_setup = "https://podcasts.google.com/?hl=pl"
 
 # Constructing parsed soup through requests [get().text return the components of page, simple get() returns code]
 prepared_soup = BeautifulSoup(requests.get(URL_setup).text, 'lxml')
 
-for episodes in prepared_soup.find_all('a', {'role': 'listitem'}):
-    # Title of creators show + subtitle of episodes - in case for further validation
-    title = prepared_soup.find('title').text
-    full_title = title + " " + episodes.find('div', {'class': 'e3ZUqe'}).text
-    title_list_all.append(full_title)
+meta_list = prepared_soup.find_all('a', {'class': 'c9x52d'})
+# test = prepared_soup.find('div', {'jsname': 'X7oyne'})['jsdata']
 
-    # Also through div tags and classes if the first is not working
-    # title = soup.find('div', {'class':'ZfMIwb'}).text
+for meta in meta_list:
+    url_main_list.append("https://podcasts.google.com" + meta['href'][1:-1])
+    channels_title_list.append(meta.find('div', {'class': 'eWeGpe'}).text)
 
-    # Extracting certain links to podcasts through find and JsName tags and splitting from the rest of data
-    link = episodes.find('div', {'jsname': 'fvi9Ef'})['jsdata'].split(';')[1]
-    link_list_all.append(link)
+for iteration, channels in enumerate(url_main_list):
 
-    # Upload time through class
-    upload_time = episodes.find('div', {'class': 'OTz6ee'}).text
-    time_list_all.append(upload_time)
+    current_url = channels
+    channel_soup = BeautifulSoup(requests.get(current_url).text, 'lxml')
 
-    dlp_scraper.download(link)
+    os.mkdir(channels_title_list[iteration])
+    os.chdir(channels_title_list[iteration])
 
-    print(title)
+    for episodes in channel_soup.find_all('a', {'role': 'listitem'}):
 
-print(prepared_soup)
+        print(channel_soup.find_all('a', {'role': 'listitem'}))
+        # Title of creators show + subtitle of episodes - in case for further validation
+        full_title = episodes.find('div', {'class': 'e3ZUqe'}).text
+        title_list_all.append(full_title)
+
+        # Also through div tags and classes if the first is not working
+        # title = soup.find('div', {'class':'ZfMIwb'}).text
+
+        # Extracting certain links to podcasts through find and JsName tags and splitting from the rest of data
+        link = episodes.find('div', {'jsname': 'fvi9Ef'})['jsdata'].split(';')[1]
+        link_list_all.append(link)
+
+        # Upload time through class
+        upload_time = episodes.find('div', {'class': 'OTz6ee'}).text
+        time_list_all.append(upload_time)
+
+        dlp_scraper.download(link)
+
+    os.chdir("..")
