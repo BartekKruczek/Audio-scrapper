cfafe0cbce8bcbd9ebf57ec250ede9a5bf9e5741
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-21 14:58:35 +0200

    Dodatkowe
    Ulepszenie przepływu informacji

---
+++ BK_test.py
@@ -0,0 +1,8 @@
+# 1 - pobieranie linku w formie string z linku do yt
+
+# import requests
+
+# x = requests.get("https://youtube.com/playlist?list=PLIM2IXHjLzGMA1NjX1-_mizbkiNhaydHt")
+# print(x.url)
+
+# 2 - pobieranie listy linków w postaci stringów z playlisty
