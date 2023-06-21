4ff57a78347cd841547fd4863d57fbcd854826ab
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-21 09:57:09 +0200

    Update BK_najszybszy.py

--- BK_najszybszy.py
+++ BK_najszybszy.py
@@ -1,12 +1,16 @@
 import yt_dlp
 import os
 
-URLS = [
-    "https://www.youtube.com/watch?v=BaW_jenozKc",
-    "https://www.youtube.com/watch?v=xpYw22NaMUc&ab_channel=PrzemekGórczykPodcast",
-]
-output = "C:/Users/krucz/Documents/Projekty/Anonimowi-Akustycy/Nagrania"
+# URLS = [
+#     "https://www.youtube.com/watch?v=BaW_jenozKc",
+#     "https://www.youtube.com/watch?v=xpYw22NaMUc&ab_channel=PrzemekGórczykPodcast",
+# ]
 
+output = "C:/Users/krucz/Documents/Projekty/Anonimowi-Akustycy/Nagrania"
+playlist_urls = "https://www.youtube.com/watch?v=BaW_jenozKc"
+URLS = (
+    []
+)  # chcemy dodać tu wszystkie linki z playlistu, potem w pętli z tąd wszystko pobrać
 
 try:
     ydl_opts = {
@@ -22,10 +26,17 @@ try:
             output, "%(title)s.%(ext)s"
         ),  # zewnętrzna ścieżka zapisu
         "ignoreerrors": True,
+        "quiet": True,
     }
+
+    # pobieranie linków z całej playlisty
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        playlist_info = ydl.extract_info(playlist_urls, download=False)
+
+        # for link in playlist_info
+
 except Exception:
     print("Coś nie działa byczqu")
 finally:
     for i in URLS:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            error_code = ydl.download(i)
+        error_code = ydl.download(i)
