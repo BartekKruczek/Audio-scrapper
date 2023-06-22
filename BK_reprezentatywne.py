ed0b71c9353fc48ca5956f879d955a5e3bf42f6d
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-22 10:23:18 +0200

    Dalsze poprawki

--- BK_reprezentatywne.py
+++ BK_reprezentatywne.py
@@ -3,10 +3,13 @@ import os
 import random
 import webvtt
 import time
+import glob
+from youtubesearchpython import *
 
 try:
 
     def download_playlist_audio(playlist_url, output_path):
+        URLS = []
         ydl_opts = {
             "format": "m4a/bestaudio/best",
             "postprocessors": [
@@ -16,9 +19,9 @@ try:
                     "preferredquality": "192",
                 }
             ],
-            "writesubtitles": True,  # pobieranie transkrypcji
+            "writesubtitles": False,  # pobieranie transkrypcji
             "writeautomaticsub": False,  # pobieranie automatycznie generowanej transkrypcji
-            "subtitleslangs": ["pl"],  # Wybór języka transkrypcji
+            "subtitleslangs": ["en"],  # Wybór języka transkrypcji
             "outtmpl": os.path.join(
                 output_path, "%(title)s_%(upload_date)s_%(timestamp)s.%(ext)s"
             ),
@@ -26,44 +29,72 @@ try:
             "n_threads": 4,
         }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            playlist_info = ydl.extract_info(playlist_url, download=False)
-            playlist_title = playlist_info["title"]
-            print(f"Pobieranie audio z playlisty: {playlist_title}")
+        # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        #     playlist_info = ydl.extract_info(playlist_url, download=False)
+        #     playlist_title = playlist_info["title"]
+        #     print(f"Pobieranie audio z playlisty: {playlist_title}")
 
-            for video in playlist_info["entries"]:
-                video_title = video["title"]
-                video_url = video["url"]
+        #     for video in playlist_info["entries"]:
+        #         video_title = video["title"]
+        #         video_url = video["url"]
 
-                print(f"Pobieranie audio z filmu: {video_title}")
+        #         print(f"Pobieranie audio z filmu: {video_title}")
 
-                # Pobieranie pliku audio
-                ydl.download([video_url])
+        #         # Pobieranie pliku audio
+        #         ydl.download([video_url])
 
-                # żywcem mnie nie wezmą!
-                delay = random.uniform(5, 10)
-                time.sleep(delay)
+        #         # żywcem mnie nie wezmą!
+        #         delay = random.uniform(5, 10)
+        #         time.sleep(delay)
 
-                # zamiana plików .vtt z transkrypcją na pliki .txx + usuwanie tych pierwszych
-                vtt_filename = os.path.join(output_path, f"{video_title}.vtt")
-                txt_filename = os.path.join(output_path, f"{video_title}.txt")
+        #         # zamiana plików .vtt z transkrypcją na pliki .txx + usuwanie tych pierwszych
+        #         vtt_filename = os.path.join(output_path, f"{video_title}.vtt")
+        #         txt_filename = os.path.join(output_path, f"{video_title}.txt")
 
-                if os.path.exists(vtt_filename):
-                    captions = webvtt.read(vtt_filename)
-                    captions.save(txt_filename, format="txt")
+        #         if os.path.exists(vtt_filename):
+        #             captions = webvtt.read(vtt_filename)
+        #             captions.save(txt_filename, format="txt")
 
-                # Usunięcie pliku .vtt
-                if os.path.exists(vtt_filename):
-                    os.remove(vtt_filename)
+        #         # Usunięcie pliku .vtt
+        #         if os.path.exists(vtt_filename):
+        #             os.remove(vtt_filename)
+
+        playlistVideos = Playlist.getVideos(playlist_url)
+
+        # wyodrębnianie url
+        for key in playlistVideos:
+            value = playlistVideos[key]
+            # print(value)
+
+        for video in value:
+            url = video["link"]
+            URLS.append(str(url))
+
+        # pobieranie
+
+        if len(URLS) > 0:
+            downloaded_files = []
+            while len(URLS) > 0:
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    if len(glob.glob(output_path + "*.wav")) != len(downloaded_files):
+                        i = URLS[0]  # Pobierz pierwszy URL z listy
+                        ydl.download(i)
+                        downloaded_files.append(i)
+                        URLS.remove(i)  # Usuń pobrany URL z listy URLS
+                    else:
+                        break
+            print("Pobrano wszystkie pliki")
+        else:
+            print("Brak URLS do pobrania")
 
 except Exception as e:
     print(str(e))
 finally:
-    # Przykładowe użycie funkcji dla playlisty na YouTube
-    # playlist_url = "https://youtube.com/playlist?list=PLIM2IXHjLzGMA1NjX1-_mizbkiNhaydHt"
-    playlist_url = "https://www.youtube.com/watch?v=Ofo8ULP-6cI&list=PL6-nym1-0TdWnICiAzd6CUXCg2crQ18Yq&index=5&ab_channel=Astrofaza"
+    playlist_url = (
+        "https://youtube.com/playlist?list=PL6-nym1-0TdWnICiAzd6CUXCg2crQ18Yq"
+    )
     output_path = (
-        "C:/Users/krucz/Documents/Projekty/Anonimowi-Akustycy/Nagrania"  # dysk lokalny
+        "C:/Users/krucz/Documents/GitHub/Anonimowi-Akustycy/Nagrania"  # dysk lokalny
     )
     # output_path = "/home/praktyki/workspace/30-stopni-w-cieniu/C:/Users/krucz/Documents/Projekty/Anonimowi-Akustycy/Nagrania"  # serwer ZPS
     download_playlist_audio(playlist_url, output_path)
--- BK_test.py
+++ BK_test.py
@@ -6,17 +6,126 @@
 # print(x.url)
 
 # 2 - pobieranie listy linków w postaci stringów z playlisty
+# from youtubesearchpython import *
+# import yt_dlp
+
+
+# URLS = []
+
+# try:
+#     ydl_opts = {
+#         "format": "m4a/bestaudio/best",
+#         # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+#         "postprocessors": [
+#             {  # Extract audio using ffmpeg
+#                 "key": "FFmpegExtractAudio",
+#                 "preferredcodec": "wav",
+#             }
+#         ],
+#     }
+
+#     playlistVideos = Playlist.getVideos(
+#         "https://youtube.com/playlist?list=PL6-nym1-0TdWnICiAzd6CUXCg2crQ18Yq"
+#     )
+
+#     # wyodrębnianie url
+#     for key in playlistVideos:
+#         value = playlistVideos[key]
+#         print(value)
+
+#     for video in value:
+#         url = video["link"]
+#         # print(str(url))
+#         URLS.append(str(url))
+# except Exception:
+#     print("Coś nie działa byczqu")
+# finally:
+#     for i in URLS:
+#         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#             error_code = ydl.download(i)
+
+# 3 - zatrzymywanie pętli
 from youtubesearchpython import *
+import yt_dlp
 
-playlistVideos = Playlist.getVideos(
-    "https://youtube.com/playlist?list=PL6-nym1-0TdWnICiAzd6CUXCg2crQ18Yq"
-)
-# print(playlistVideos)
+URLS = []
 
-# for video in playlistVideos:
-#     video_url = video["link"]
-#     print(video_url)
+try:
+    ydl_opts = {
+        "format": "m4a/bestaudio/best",
+        # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        "postprocessors": [
+            {  # Extract audio using ffmpeg
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+            }
+        ],
+    }
 
-for key in playlistVideos:
-    value = playlistVideos[key]
-    print(value)
+    playlistVideos = Playlist.getVideos(
+        "https://youtube.com/playlist?list=PL6-nym1-0TdWnICiAzd6CUXCg2crQ18Yq"
+    )
+
+    # wyodrębnianie url
+    for key in playlistVideos:
+        value = playlistVideos[key]
+        print(value)
+
+    for video in value:
+        url = video["link"]
+        URLS.append(str(url))
+
+except Exception:
+    print("Coś nie działa byczqu")
+
+finally:
+    if len(URLS) > 0:
+        for i in URLS:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                error_code = ydl.download(i)
+                if error_code != 0:
+                    break
+    else:
+        print("Brak URLS do pobrania")
+
+# 4 - zatrzymanie pętli
+# from youtubesearchpython import *
+# import yt_dlp
+
+# URLS = []
+
+# try:
+#     ydl_opts = {
+#         "format": "m4a/bestaudio/best",
+#         # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+#         "postprocessors": [
+#             {  # Extract audio using ffmpeg
+#                 "key": "FFmpegExtractAudio",
+#                 "preferredcodec": "wav",
+#             }
+#         ],
+#     }
+
+#     playlistVideos = Playlist.getVideos(
+#         "https://youtube.com/playlist?list=PL6-nym1-0TdWnICiAzd6CUXCg2crQ18Yq"
+#     )
+
+#     # wyodrębnianie url
+#     for key in playlistVideos:
+#         value = playlistVideos[key]
+#         print(value)
+
+#     for video in value:
+#         url = video["link"]
+#         URLS.append(str(url))
+
+#     if len(URLS) > 0:
+#         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#             error_code = ydl.download(URLS)
+#             if error_code != 0:
+#                 print("Błąd podczas pobierania.")
+#     else:
+#         print("Brak URLS do pobrania")
+
+# except Exception as e:
+#     print("Coś nie działa byczqu:", str(e))
