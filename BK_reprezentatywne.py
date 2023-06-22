17beaec2f337de6a280fcc047059399a5b99ed07
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-22 14:31:02 +0200

    Update BK_reprezentatywne.py
    Jak na razie wersja oficjalna

--- BK_reprezentatywne.py
+++ BK_reprezentatywne.py
@@ -1,15 +1,17 @@
 import yt_dlp
 import os
 import random
-import webvtt
 import time
 import glob
 from youtubesearchpython import *
+from youtube_transcript_api import YouTubeTranscriptApi
 
 try:
+    kompendium = {}
+    URLS = []
+    Ids = []
 
-    def download_playlist_audio(playlist_url, output_path):
-        URLS = []
+    def download_playlist_audio(playlist_url, output_path, download):
         ydl_opts = {
             "format": "m4a/bestaudio/best",
             "postprocessors": [
@@ -19,27 +21,12 @@ try:
                     "preferredquality": "192",
                 }
             ],
-            "writesubtitles": False,  # pobieranie transkrypcji
-            "writeautomaticsub": False,  # pobieranie automatycznie generowanej transkrypcji
-            "subtitleslangs": ["en"],  # Wybór języka transkrypcji
             "outtmpl": os.path.join(
                 output_path, "%(title)s_%(upload_date)s_%(timestamp)s.%(ext)s"
             ),
             "ignoreerrors": True,
             "n_threads": 4,
         }
-
-        #         # zamiana plików .vtt z transkrypcją na pliki .txx + usuwanie tych pierwszych
-        #         vtt_filename = os.path.join(output_path, f"{video_title}.vtt")
-        #         txt_filename = os.path.join(output_path, f"{video_title}.txt")
-
-        #         if os.path.exists(vtt_filename):
-        #             captions = webvtt.read(vtt_filename)
-        #             captions.save(txt_filename, format="txt")
-
-        #         # Usunięcie pliku .vtt
-        #         if os.path.exists(vtt_filename):
-        #             os.remove(vtt_filename)
 
         playlistVideos = Playlist.getVideos(playlist_url)
 
@@ -53,20 +40,51 @@ try:
             URLS.append(str(url))
 
         # pobieranie
-        if len(URLS) > 0:
-            for i in URLS:
-                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                    if len(glob.glob(output_path + "*.wav")) != len(URLS):
-                        ydl.download(i)
-                        delay = random.uniform(5, 10)
-                        time.sleep(delay)
-                    else:
+        if download == True:
+            if len(URLS) > 0:
+                for i in URLS:
+                    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                        if len(glob.glob(output_path + "*.wav")) != len(URLS):
+                            ydl.download(i)
+                            delay = random.uniform(5, 10)
+                            time.sleep(delay)
+                        else:
+                            break
+                        print("Pobrano wszystkie pliki")
                         break
-                    print("Pobrano wszystkie pliki")
-                    break
-            URLS.remove(i)
+                URLS.remove(i)
+            else:
+                print("Brak URLS do pobrania")
         else:
-            print("Brak URLS do pobrania")
+            # print(URLS)
+            pass
+
+    def extracting_id():
+        for info in URLS:
+            videoInfo = Video.getInfo(info, mode=ResultMode.json)
+            # print(videoInfo)
+            value = videoInfo["id"]
+            Ids.append(value)
+        # print(str(Ids))
+
+    def download_transcription():
+        for key in kompendium.keys():
+            try:
+                # print(key)  # debug
+                transcript_list = YouTubeTranscriptApi.list_transcripts(key)
+                transcript = transcript_list.find_manually_created_transcript(["pl"])
+                if transcript is not None:
+                    print(str(transcript))
+                else:
+                    print("Brak dostępnej transkrypcji")
+                    continue
+            except Exception as e:
+                print(str(e))
+
+    def combining_all():
+        global kompendium
+        kompendium = dict(zip(Ids, URLS))
+        return kompendium
 
 except Exception as e:
     print(str(e))
@@ -74,8 +92,15 @@ finally:
     playlist_url = (
         "https://youtube.com/playlist?list=PLIM2IXHjLzGMA1NjX1-_mizbkiNhaydHt"
     )
-    # output_path = (
-    #     "C:/Users/krucz/Documents/GitHub/Anonimowi-Akustycy/Nagrania"  # dysk lokalny
-    # )
-    output_path = "/mnt/w01/praktyki/30-stopni-w-cieniu/Nagrania"  # serwer ZPS
-    download_playlist_audio(playlist_url, output_path)
+    output_path = (
+        "C:/Users/krucz/Documents/GitHub/Anonimowi-Akustycy/Nagrania"  # dysk lokalny
+    )
+    # output_path = "/mnt/w01/praktyki/30-stopni-w-cieniu/Nagrania"  # serwer ZPS
+    output_file = "transcript.txt"
+    download_playlist_audio(
+        playlist_url, output_path, False
+    )  # argument boolean determinuje czy pobieramy czy tylko ekstrahujemy linki
+    extracting_id()
+    combining_all()
+    print(combining_all())
+    download_transcription()
