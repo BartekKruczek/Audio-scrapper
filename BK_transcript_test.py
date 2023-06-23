391589a6b2dd36eec39ce6301056d0b6bfc38e91
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-23 09:56:44 +0200

    Update BK_transcript_test.py

--- BK_transcript_test.py
+++ BK_transcript_test.py
@@ -1,11 +1,13 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import TranscriptsDisabled
 
 video_ids = ["OHf4tcgcX2M", "ANkWjOx2d3A", "CDBVdVHTr_w"]  # lista video IDs
 
 for video_id in video_ids:
-    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-    transcript = transcript_list.find_manually_created_transcript(["pl"])
     try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript = transcript_list.find_manually_created_transcript(["pl"])
+
         if transcript is not None:
             lines = []
             for line in transcript.fetch():
@@ -22,7 +24,5 @@ for video_id in video_ids:
                 text_file.write(text_formatted)
 
             print(f"Transkrypcja dla video ID {video_id} została zapisana.")
-        else:
-            print(f"Brak dostępnej transkrypcji po polsku dla video ID {video_id}.")
-    except Exception as e:
-        print(str(e))
+    except TranscriptsDisabled:
+        pass
