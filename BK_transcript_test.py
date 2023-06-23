24fe588c3117f9051713592edf4f03134d3f7ea0
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-23 09:51:41 +0200

    Dalsza transkrypcja

--- BK_transcript_test.py
+++ BK_transcript_test.py
@@ -1,22 +1,28 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-video_id = "OHf4tcgcX2M"
+video_ids = ["OHf4tcgcX2M", "ANkWjOx2d3A", "CDBVdVHTr_w"]  # lista video IDs
 
-# retrieve the available transcripts
-transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+for video_id in video_ids:
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+    transcript = transcript_list.find_manually_created_transcript(["pl"])
+    try:
+        if transcript is not None:
+            lines = []
+            for line in transcript.fetch():
+                text = line["text"]
+                start = line["start"]
+                duration = line["duration"]
 
-# iterate over all available transcripts
-for transcript in transcript_list:
-    lines = []
-    for line in transcript.fetch():
-        text = line["text"]
-        start = line["start"]
-        duration = line["duration"]
+                line_with_timestamp = f"[{duration}] [{start}] {text}"
+                lines.append(line_with_timestamp)
 
-        line_with_timestamp = f"[{start}] {text} [{duration}]"
-        lines.append(line_with_timestamp)
+            text_formatted = "\n".join(lines)
 
-    text_formatted = "\n".join(lines)
+            with open(f"{video_id}_transcript.txt", "w", encoding="utf-8") as text_file:
+                text_file.write(text_formatted)
 
-    with open("your_filename.txt", "w", encoding="utf-8") as text_file:
-        text_file.write(text_formatted)
+            print(f"Transkrypcja dla video ID {video_id} została zapisana.")
+        else:
+            print(f"Brak dostępnej transkrypcji po polsku dla video ID {video_id}.")
+    except Exception as e:
+        print(str(e))
