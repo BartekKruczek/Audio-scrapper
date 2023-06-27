8819c1f40b80ecc23aebc23365036f2e5b3f2d1a
JKChojnacki <jchojnackitm@gmail.com>
2023-06-27 14:58:43 +0200

    Simultaneous usage of whisper and faster-whisper

--- dlp_slicer.py
+++ dlp_slicer.py
@@ -7,6 +7,7 @@ import requests
 import time
 #import pycld2 as cld2
 
+import whisper
 from faster_whisper import WhisperModel
 
 from fake_useragent import UserAgent
@@ -17,13 +18,14 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 from pydub.utils import db_to_float
 
-URL='https://www.youtube.com/watch?v=i-NVAhJXw44' # plik bez napisów
-#URL='https://www.youtube.com/watch?v=_SCSSLGZcSE' # plik posiadający napisy autora
+#URL='https://www.youtube.com/watch?v=i-NVAhJXw44' # plik bez napisów
+URL='https://www.youtube.com/watch?v=_SCSSLGZcSE' # plik posiadający napisy autora
 #URL='https://www.youtube.com/watch?v=S2Ww3rX-piw' # plik z tłem
 
 path=os.getcwd()+"\\nagrania\\"
 
-model = WhisperModel("medium",device='cpu', compute_type="int8")
+#model = WhisperModel("medium",device='cpu', compute_type="int8")
+model = whisper.load_model("small",device='cuda')
 
 def cleaning_wavs(path):
     for fileName in os.listdir(Path(path)):
@@ -42,7 +44,7 @@ if os.path.isdir(path)==True:
 
 ydl_opts = {
     'format': 'wav/bestaudio/best',
-    'outtmpl': path+'\%(title)s.%(ext)s',
+    'outtmpl': path+'%(title)s.%(ext)s',
     #'proxy': '139.144.24.46:8080',
     #'proxy': '82.145.46.190:3128',
     #'proxy': '20.219.111.119:8080',
@@ -64,18 +66,18 @@ with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
 filename=filename_ext(path)
 
-# print(requests.get('https://ident.me').text)
+print(requests.get('https://ident.me').text)
 
-# proxies = {
-#     'http': 'socks5h://localhost:9150',
-#     'https': 'socks5h://localhost:9150'
-# }
+proxies = {
+     'http': 'socks5h://localhost:9150',
+     'https': 'socks5h://localhost:9150'
+}
 
-# headers = { 'User-Agent': UserAgent().random }
-# with Controller.from_port(port = 9051) as c:
-#             c.authenticate()
-#             c.signal(Signal.NEWNYM)
-#             print(f"Your IP is : {requests.get('https://ident.me', proxies=proxies, headers=headers).text}  ||  User Agent is : {headers['User-Agent']}")
+headers = { 'User-Agent': UserAgent().random }
+with Controller.from_port(port = 9051) as c:
+            c.authenticate()
+            c.signal(Signal.NEWNYM)
+            print(f"Your IP is : {requests.get('https://ident.me', proxies=proxies, headers=headers).text}  ||  User Agent is : {headers['User-Agent']}")
 
 # with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 #     error_code = ydl.download(URL)
@@ -98,7 +100,7 @@ audio_chunks = split_on_silence(sound_file,
 )
 for i, chunk in enumerate(audio_chunks):
 
-    out_file = path+"./splits_"+ filename.strip('.wav')+"/split{0}.wav".format(i)
+    out_file = path+"splits_"+ filename.strip('.wav')+"\split{0}.wav".format(i)
     print("exporting "+ out_file)
     chunk.export(Path(out_file), format="wav")
     #audio=whisper.load_audio(Path(out_file))
@@ -109,6 +111,18 @@ for i, chunk in enumerate(audio_chunks):
     #audio_lang=list(lang_dic.keys())[list(lang_dic.values()).index(audio_lang_value)]
     #print(audio_lang)
 # results,info = model.transcribe(path+filename, beam_size=5)
+result=model.transcribe(path+filename,word_timestamps=True)
+audio=whisper.load_audio(Path(path+filename))
+audio=whisper.pad_or_trim(audio)
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+lang_dic=model.detect_language(mel)[1]
+audio_lang_value=max(lang_dic.values())
+audio_lang=list(lang_dic.keys())[list(lang_dic.values()).index(audio_lang_value)]
+print(audio_lang)
+with open(Path(path+filename.strip('.wav')+'.txt'), 'w') as f:
+    for segment in result['segments']:
+        f.write(f"{segment['start']}_{segment['end']}_{segment['text']}\n")
+    f.close()
 # for result in results:
 #     with open(Path(path+filename.strip('.wav')+'.txt'), 'w') as f:
 #              f.write(f"{result.start},{result.end},{result.text}\n")
