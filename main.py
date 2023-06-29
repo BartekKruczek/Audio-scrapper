084e03e7eca63ef5552eb7dffe5d541375e53a5c
JacobeCode <120515248+JacobeCode@users.noreply.github.com>
2023-06-29 13:24:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,3 +21,18 @@ scrapper.download_transcription(output_path, False)
 processor = Process()
 processor.transcribe_folder(output_path,whisper_model_size='base', preferred_device='cpu',language_detection=False)
 processor.split_to_words_folder(output_path,whisper_model_size='base', preferred_device='cpu')
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# gp_operator part
+
+cur_valid_path = "Python_scripts/valid_ip_list.txt"
+cur_raw_path = "Python_scripts/raw_ip_list.txt"
+cur_agents_path = "Python_scripts/static_agents_list.txt"
+storage_path = "/mnt/s01/praktyki/"
+overwrite = True
+
+validator = ip_validator(cur_valid_path, cur_raw_path, overwrite)
+gp_op = gp_operator(cur_valid_path, cur_agents_path, storage_path, True, True)
+
+validator.ip_validate()
+gp_op.audio_extraction()
