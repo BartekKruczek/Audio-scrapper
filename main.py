ed4ae7e6e43eac5601d3a61bec31127f7239f333
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2023-06-29 14:11:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,7 @@ from Process import *
 from ip_tester_2_0 import *
 from gp_operations import *
 
-playlist_urls = [
-    "https://youtube.com/playlist?list=PLUWDBVpNIE52-QW1DuVyQu-QWLCtIGgJX",
-    "https://youtube.com/playlist?list=PLUWDBVpNIE51lQ96yID-oF-IKppUNrpay",
-    "https://youtube.com/playlist?list=PLUWDBVpNIE51d-kaYJyIOIE9fm7RNqI8C",
-    "https://youtube.com/playlist?list=PLTld5jYla5hbtbhnUNXCrD7p-w-wWI2xP",
-    "https://youtube.com/playlist?list=PL6-nym1-0TdWnICiAzd6CUXCg2crQ18Yq",
-    "https://youtube.com/playlist?list=PL6-nym1-0TdULhklxX-97X28UXiKiUYop",
-    "https://youtube.com/playlist?list=PL6-nym1-0TdUD0t7tbGEjs6lQcvuicoQH",
-]
+playlist_urls = ["playlist_url_example", "playlist_url_example_2"]
 output_path = "your_path"
 proxy_path = "your_proxy_path.txt"
 
